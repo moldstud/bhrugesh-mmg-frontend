7a0b44d2b85db643b73b2e2a109b5eeef733192f
moldstud-hr <reg@moldstud.com>
2019-06-25 16:29:16 +0530

    read me updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,44 +1,43 @@
-/* eslint-disable indent */
 const currentEnv = process.env.NODE_ENV;
 
 const dev = {
-  API_HOST:
-    'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
+	API_HOST:
+		'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
 };
 const prod = {
-  API_HOST:
-    'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
+	API_HOST:
+		'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
 };
 const uat = {
-  API_HOST:
-    'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
+	API_HOST:
+		'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
 };
 const qa = {
-  API_HOST:
-    'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
+	API_HOST:
+		'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
 };
 const staging = {
-  API_HOST:
-    'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
+	API_HOST:
+		'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json'
 };
 
 const config =
-  currentEnv === 'production'
-    ? prod
-    : currentEnv === 'development'
-    ? dev
-    : currentEnv === 'uat'
-    ? uat
-    : currentEnv === 'qa'
-    ? qa
-    : staging;
+	currentEnv === 'production'
+		? prod
+		: currentEnv === 'development'
+		? dev
+		: currentEnv === 'uat'
+		? uat
+		: currentEnv === 'qa'
+		? qa
+		: staging;
 
 const baseOptions = {
-  url: config.API_HOST,
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  }
+	url: config.API_HOST,
+	headers: {
+		'Content-Type': 'application/json',
+		Accept: 'application/json'
+	}
 };
 
 export { currentEnv, config, baseOptions };
